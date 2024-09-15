7a9aa1ecdaebc3d01531662cd847b96f504aa33c
Madhav <mishramadhav2468@gmail.com>
2024-09-15 18:17:48 +0530

    css modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,6 @@ import { files } from "../utils/Constants";
 import Contact from "./Contact";
 import { isMobile } from "react-device-detect";
 
-
 // Register GSAP plugins
 gsap.registerPlugin(ScrollTrigger);
 
@@ -69,9 +68,11 @@ function Hero() {
     const context = canvas.getContext("2d");
 
     let frameCount = 350;
-    if(isMobile){
-         frameCount = 350;
-    }else{ frameCount = 350;}
+    if (isMobile) {
+      frameCount = 350;
+    } else {
+      frameCount = 350;
+    }
     const images2 = [];
     const imageSeq = { frame: 1 };
 
@@ -85,36 +86,35 @@ function Hero() {
       };
       images2.push(img);
     }
-    if(isMobile){
-        gsap.to(imageSeq, {
-          frame: frameCount - 1,
-          snap: "frame",
-          ease: "none",
-          scrollTrigger: {
-            scrub: 0.05,
-            trigger: canvas,
-            start: "top top",
-            end: "600% top",
-            scroller: mainRef.current,
-          },
-          onUpdate: render,
-        });
-     }else{
-        gsap.to(imageSeq, {
-          frame: frameCount - 1,
-          snap: "frame",
-          ease: "none",
-          scrollTrigger: {
-            scrub: 0.05,
-            trigger: canvas,
-            start: "top top",
-            end: "600% top",
-            scroller: mainRef.current,
-          },
-          onUpdate: render,
-        });
-     }
-    
+    if (isMobile) {
+      gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: "none",
+        scrollTrigger: {
+          scrub: 0.05,
+          trigger: canvas,
+          start: "top top",
+          end: "600% top",
+          scroller: mainRef.current,
+        },
+        onUpdate: render,
+      });
+    } else {
+      gsap.to(imageSeq, {
+        frame: frameCount - 1,
+        snap: "frame",
+        ease: "none",
+        scrollTrigger: {
+          scrub: 0.05,
+          trigger: canvas,
+          start: "top top",
+          end: "600% top",
+          scroller: mainRef.current,
+        },
+        onUpdate: render,
+      });
+    }
 
     function scaleImage(img, ctx) {
       const canvas = ctx.canvas;
@@ -137,7 +137,19 @@ function Hero() {
       const centerShiftY = 0;
 
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+     if(isMobile){
+      ctx.drawImage(
+        img,
+        680,
+        0,
+        img.width,
+        img.height,
+        centerShiftX,
+        centerShiftY,
+        (imgWidth = 1600),
+        (imgHeight = 900)
+      );
+    }else{
       ctx.drawImage(
         img,
         0,
@@ -149,6 +161,9 @@ function Hero() {
         imgWidth,
         imgHeight
       );
+      
+    }
+      
     }
 
     function render() {
@@ -156,36 +171,37 @@ function Hero() {
         scaleImage(images2[imageSeq.frame], context);
       }
     }
-if(isMobile){
-
-    ScrollTrigger.create({
+    // Create ScrollTriggers based on whether the device is mobile or not
+    if (isMobile) {
+      ScrollTrigger.create({
         trigger: canvas,
         pin: true,
         scroller: mainRef.current,
         start: "top top",
-        end: "280% top",
-    });
-}else{
-    ScrollTrigger.create({
-      trigger: canvas,
-      pin: true,
-      scroller: mainRef.current,
-      start: "top top",
-      end: "600% top",
-    });
-    
-}
-    
+        end: "550% top",
+      });
+    } else {
+      ScrollTrigger.create({
+        trigger: canvas,
+        pin: true,
+        scroller: mainRef.current,
+        start: "top top",
+        end: "600% top",
+      });
+    }
+
+    // Pinning for #page1
     gsap.to("#page1", {
-        scrollTrigger: {
-            trigger: "#page1",
-            start: "top top",
-            end: "bottom top",
-            pin: true,
-            scroller: mainRef.current,
-        },
+      scrollTrigger: {
+        trigger: "#page1",
+        start: "top top",
+        end: "bottom top",
+        pin: true,
+        scroller: mainRef.current,
+      },
     });
 
+    // Pinning for #page2
     gsap.to("#page2", {
       scrollTrigger: {
         trigger: "#page2",
@@ -196,13 +212,14 @@ if(isMobile){
       },
     });
 
+    // Pinning for #page3 with different configurations for mobile and desktop
     gsap.to("#page3", {
       scrollTrigger: {
         trigger: "#page3",
         start: "top top",
-        end: "bottom top",
         pin: true,
         scroller: mainRef.current,
+        end: isMobile ? "460px top" : "100% 80%",
       },
     });
 
@@ -218,85 +235,78 @@ if(isMobile){
   }, []);
 
   return (
-    
-        <div>
-    <div ref={mainRef} id="main">
-      <div id="page">
-        <div id="loop" className="flex justify-center items-center ">
-          <h1 className="text-center sm:text-2xl md:text-[16px] font-bold mb-8 sm:mb-12 md:mb-20">
-            <b>YOU</b> SEE THE{" "}
-            <b>
-              <i>WORLD</i>
-            </b>{" "}
-            <span>AS </span>BINARY,{" "}
-            <span>
-              <i> WHEN IT'S MOSIAC.</i>
-            </span>
-          </h1>
+    <div>
+      <div ref={mainRef} id="main">
+        <div id="page">
+          <div id="loop" className="flex justify-center items-center ">
+            <h1 className="flex text-center sm:text-[sm] sm:mr-4 md:text-lg font-bold mb-4 sm:mb-6 md:mb-8">
+              <b className="text-center  text-base sm:text-[sm] sm:mr-4 md:text-lg font-bold mb-4 sm:mb-6 md:mb-8">
+                <i class="quattrocento-bold text-sm ">
+                  YOU SEE THE WORLD AS BINARY, WHEN IT'S MOSIAC.
+                </i>{" "}
+              </b>
+            </h1>
+          </div>
+
+          <canvas id="canvas-container" ref={canvasRef}></canvas>
+        </div>
+        <div id="page1">
+          <div id="right-text">
+            <h3>A STORY I ALWAYS IMAGINED</h3>
+            <h1>
+              DONT LIMIT YOURSELF
+              <br />
+              SOUNDS CORNY
+              <br />
+              BUT IT'S THE TRUTH
+            </h1>
+          </div>
+          <div id="left-text">
+            <h1>
+              MAKE A STORY
+              <br />
+              TAKE A CHANCE
+              <br />
+              BUILD AND OWN
+            </h1>
+            <h3>..AND MAINTAIN GOOD HUMANITY</h3>
+          </div>
+        </div>
+        <div id="page2">
+          <div id="text1">
+            <h3>A SUBLIMAL CONCEPT</h3>
+            <h1>
+              OF
+              <br />
+              HOW
+              <br />
+              REALITY WORKS
+            </h1>
+          </div>
+          <div id="text2">
+            <p>
+              FIND WHAT LIGHTS THE FIRE INSIDE YOU, <br />
+              AND GIVE IT MORE FUEL; OTHERWISE, YOU MAY JUST BURN OUT.
+            </p>
+          </div>
+        </div>
+        <div id="page3">
+          <div id="text3">
+            <h3>TEACHING MYSELF </h3>
+            <h1>
+              HOW TO ROLL
+              <br />
+              WITH
+              <br />
+              THE CHANGES.
+            </h1>
+          </div>
         </div>
 
-        <canvas id="canvas-container" ref={canvasRef}></canvas>
+        <Contact />
+        <Footer />
       </div>
-      <div id="page1">
-        <div id="right-text">
-          <h3>A STORY I ALWAYS IMAGINED</h3>
-          <h1>
-            DONT LIMIT YOURSELF
-            <br />
-            SOUNDS CORNY
-            <br />
-            BUT IT'S THE TRUTH
-          </h1>
-        </div>
-        <div id="left-text">
-          <h1>
-            MAKE A STORY
-            <br />
-            TAKE A CHANCE
-            <br />
-            BUILD AND OWN
-          </h1>
-          <h3>..AND MAINTAIN GOOD HUMANITY</h3>
-        </div>
-      </div>
-      <div id="page2">
-        <div id="text1">
-          <h3>A SUBLIMAL CONCEPT</h3>
-          <h1>
-            OF
-            <br />
-            HOW
-            <br />
-            REALITY WORKS
-          </h1>
-        </div>
-        <div id="text2">
-          <p>
-            FIND WHAT LIGHTS THE FIRE INSIDE YOU, <br />
-            AND GIVE IT MORE FUEL; OTHERWISE, YOU MAY JUST BURN OUT.
-          </p>
-        </div>
-      </div>
-      <div id="page3">
-        <div id="text3">
-          <h3>TEACHING MYSELF </h3>
-          <h1>
-            HOW TO ROLL
-            <br />
-            WITH
-            <br />
-            THE CHANGES.
-          </h1>
-        </div>
-      </div>
- 
-
-
- <Contact/>
- <Footer/>
- </div>
-  </div>
-  
+    </div>
   );
 }
 
